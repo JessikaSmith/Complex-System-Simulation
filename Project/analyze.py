25c2b4a25fbef10f6345934b2e0224ccf8bf0b02
JessikaSmith <mariyaxod@yandex.ru>
2018-05-13 20:11:12 +0300

    Figures added

--- Project/analyze.py
+++ Project/analyze.py
@@ -1,21 +1,94 @@
+# data source: http://konect.uni-koblenz.de/networks/cit-HepPh
+IMG_PATH = 'Figures/'
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from processing import *
 
-def plot_degree_distribution():
-    pass
+plt.interactive(True)
+
 
 def create_graph(data):
-    G = nx.Graph()
+    G = nx.DiGraph()
     for i, j in data.values:
         G.add_edge(i, j)
     return G
+
+
+def plot_in_degree_distribution(graph):
+    degs = {}
+    for n in graph.nodes():
+        deg = graph.in_degree(n)
+        if deg not in degs:
+            degs[deg] = 0
+        degs[deg] += 1
+    items = sorted(degs.items())
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot([k for (k, v) in items], [v / len(graph.nodes()) for (k, v) in items], 'ro')
+    ax.set_xscale('log')
+    plt.xlabel('k')
+    ax.set_yscale('log')
+    plt.ylabel('P(k)')
+    plt.title('In-Degree Distribution')
+    fig.savefig(IMG_PATH + 'in_degree_distribution.png')
+
+
+def plot_out_degree_distribution(graph):
+    degs = {}
+    for n in graph.nodes():
+        deg = graph.out_degree(n)
+        if deg not in degs:
+            degs[deg] = 0
+        degs[deg] += 1
+    items = sorted(degs.items())
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot([k for (k, v) in items], [v / len(graph.nodes()) for (k, v) in items], 'ro')
+    ax.set_xscale('log')
+    plt.xlabel('k')
+    ax.set_yscale('log')
+    plt.ylabel('P(k)')
+    plt.title('Out-Degree Distribution')
+    fig.savefig(IMG_PATH + 'out_degree_distribution.png')
+
+
+def plot_distances(dist):
+    dist_count = {}
+    for i in range(len(dist.keys()) - 1):
+        for j in range(i, len(dist.keys())):
+            if i != j:
+                if dist[i][j] not in dist_count:
+                    dist_count[dist[i][j]] = 0
+                dist_count[dist[i][j]] += 1
+    items = sorted(dist_count.items())
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot([v for (k, v) in items], [k for (k, v) in items], 'ro')
+    plt.xlabel('Distance')
+    plt.ylabel('Number of nodes')
+    plt.title('Distance Distribution')
+    fig.savefig('distance_distribution.png')
+
+
+def get_assortativity_coeff(g):
+    return nx.degree_pearson_correlation_coefficient(g)
+
+
+def draw_graph(graph):
+    nx.draw(graph, pos=nx.spring_layout(graph), with_labels=False)
+    print("plotted")
+
 
 def main():
     fname = 'Datasets/Cit-HepPh.txt'
     data = read_txt(fname)
     graph = create_graph(data)
-    print(graph.edges())
+    # draw_graph(graph)
+    get_assortativity_coeff(graph)
+    plot_in_degree_distribution(graph)
+    plot_out_degree_distribution(graph)
+
 
 if __name__ == '__main__':
     main()
