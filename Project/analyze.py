bbea0df133f054541af31d7df8098a351c508d9b
JessikaSmith <mariyaxod@yandex.ru>
2018-05-21 01:47:48 +0300

    giant component filtered + minor changes

--- Project/analyze.py
+++ Project/analyze.py
@@ -90,7 +90,7 @@ def plot_clustering_coeffs(clust):
     plt.ylabel('Node')
     plt.xlabel('$C_i$')
     plt.title('Clustering Coefficients Distribution')
-    fig.savefig(IMG_PATH +'clust_distribution.png')
+    fig.savefig(IMG_PATH + 'clust_distribution.png')
 
 
 def plot_avg_degree_connectivity(connect):
@@ -101,22 +101,24 @@ def plot_avg_degree_connectivity(connect):
     plt.xlabel('k')
     plt.ylabel('$<k_{nn}>$')
     plt.title('Assortativity Check')
-    fig.savefig(IMG_PATH +'assortativity.png')
+    fig.savefig(IMG_PATH + 'assortativity.png')
 
 
 def get_assortativity_coeff(g):
     return nx.degree_pearson_correlation_coefficient(g)
+
 
 def estimating_power_law(data):
     values = data['Degree']
     cum_sum = 0
     for i in values:
         cum_sum += math.log(i)
-    return 1 + len(values)*(cum_sum**(-1))
+    return 1 + len(values) * (cum_sum ** (-1))
 
 
 def draw_graph(graph):
     nx.draw(graph, pos=nx.spring_layout(graph), with_labels=False)
+
 
 def plot_betweeness_centrality(data):
     values = data['betweenesscentrality']
@@ -137,20 +139,45 @@ def plot_betweeness_centrality(data):
     plt.title('Betweeness Centrality Distribution')
     fig.savefig(IMG_PATH + 'betweenesscentrality_distribution.png')
 
+
+def giant_component_filtering(graph):
+    giant = max(nx.strongly_connected_components(graph), key=len)
+    H = graph.subgraph(giant)
+    print(H)
+    nx.write_edgelist(H, "Datasets/giant_component.edgelist")
+    return H
+
+
+def convert_to_csv(fname):
+    df = pd.read_csv(fname, sep=" ", header=None)
+    df = df.ix[:,[0,1]]
+    df.to_csv("Datasets/giant_component_edges.csv", index=False, header=False)
+
+
+def graph_comparison(graph, giant_comp):
+    print(graph.number_of_nodes(), giant_comp.number_of_nodes())
+    print(graph.number_of_edges(), giant_comp.number_of_edges())
+
+
 def main():
-    fname = 'Datasets/Cit-HepPh.txt'
-    data = read_txt(fname)
+    fname = 'Datasets/Datasets/giant_component_edges.csv'
+    data = read_csv(fname)
     graph = create_graph(data)
+    # component = giant_component_filtering(graph)
+    # graph_comparison(graph, component)
+    fname = 'Datasets/giant_component.edgelist'
+    #convert_to_csv(fname)
+
     # draw_graph(graph)
-    print(get_assortativity_coeff(graph))
+    # print(get_assortativity_coeff(graph))
     # plot_in_degree_distribution(graph)
     # plot_out_degree_distribution(graph)
     # plot_distances(nx.shortest_path_length(graph))
     # plot_avg_degree_connectivity(nx.average_degree_connectivity(graph))
-    node_metrics = 'Datasets/node_metrics.csv'
-    metrics = read_csv(node_metrics)
+    # node_metrics = 'Datasets/node_metrics.csv'
+    # metrics = read_csv(node_metrics)
     # plot_betweeness_centrality(metrics)
-    print(estimating_power_law(metrics))
+    # print(estimating_power_law(metrics))
 
 
 if __name__ == '__main__':
